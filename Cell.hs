1d52f4ae6887f5c81100ec39f8992103fef8cc7e
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-11-09 23:06:00 -0800

    Flatten Maybe from rule applier functions

--- Cell.hs
+++ Cell.hs
@@ -10,40 +10,40 @@ type Rule = Map (Bool, Bool, Bool) Bool
 zeroCell = '⬛'
 oneCell = '⬜'
 
-getNext :: Rule -> [Bool] -> Maybe [Bool]
+getNext :: Rule -> [Bool] -> [Bool]
 getNext rule [] = getNext' rule False [False]
 getNext rule (left:cells) =
   case Map.lookup (False, False, left) rule of
     Just left' -> case getNext'' rule False left cells of
-                    Just cells' -> Just (left' : cells')
-                    Nothing -> Nothing
-    Nothing -> Nothing
+                    [] -> []
+                    cells' -> left' : cells'
+    Nothing -> []
 
-getNext' :: Rule -> Bool -> [Bool] -> Maybe [Bool]
+getNext' :: Rule -> Bool -> [Bool] -> [Bool]
 getNext' rule left [] =
   case Map.lookup (left, False, False) rule of
-    Just cell -> Just [cell]
-    Nothing -> Nothing
+    Just cell -> [cell]
+    Nothing -> []
 getNext' rule left (mid:cells) =
   case Map.lookup (False, left, mid) rule of
     Just left' -> case getNext'' rule left mid cells of
-                    Just cells' -> Just (left' : cells')
-                    Nothing -> Nothing
-    Nothing -> Nothing
+                    [] -> []
+                    cells' -> left' : cells'
+    Nothing -> []
 
-getNext'' :: Rule -> Bool -> Bool -> [Bool] -> Maybe [Bool]
+getNext'' :: Rule -> Bool -> Bool -> [Bool] -> [Bool]
 getNext'' rule left mid [] =
   case Map.lookup (left, mid, False) rule of
     Just mid' -> case Map.lookup (mid, False, False) rule of
-                   Just right -> Just [mid', right]
-                   Nothing -> Nothing
-    Nothing -> Nothing
+                   Just right -> [mid', right]
+                   Nothing -> []
+    Nothing -> []
 getNext'' rule left mid (right:cells) =
   case Map.lookup (left, mid, right) rule of
     Just mid' -> case getNext'' rule mid right cells of
-                   Just cells' -> Just (mid' : cells')
-                   Nothing -> Nothing
-    Nothing -> Nothing
+                   [] -> []
+                   cells' -> mid' : cells'
+    Nothing -> []
 
 genRule :: Int -> Rule
 genRule n = Map.fromList . zip allBoolTriples $ map (testBit n) [0..7]
@@ -66,9 +66,9 @@ aux :: Rule -> Int -> ([Bool], Int) -> IO (Maybe ([Bool] , Int))
 aux rule width (cells, i)
   | i <= 0 = return Nothing
   | otherwise = case getNext rule cells of
-                  Just cells -> do putStrLn $ showPadded width cells
-                                   return $ Just (cells, i - 1)
-                  Nothing -> return Nothing
+                  [] -> return Nothing
+                  cells -> do putStrLn $ showPadded width cells
+                              return $ Just (cells, i - 1)
 
 main :: IO ()
 main = do
